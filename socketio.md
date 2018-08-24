9f47f06e631e68b21f23bafe0e2ce5624fcfe3d8
Steph <tzm8334@gmail.com>
2018-08-24 15:55:49 +0800

    first commit

---
+++ socketio.md
@@ -0,0 +1,14 @@
+# socket.io 使用
+
++ 广播
+
+```javascript
+io.emit()
+```
+
++ 向 socket 代表的 client 发送消息
+
+```javascript
+socket.emit()
+```
+
---
+++ wsServer.js
@@ -0,0 +1,23 @@
+let app = require('http').createServer()
+let io = require('socket.io')(app)
+
+const PORT = 8080
+let clientCount = 0
+
+app.listen(PORT)
+
+io.on('connection', function(socket) {
+  clientCount++
+  socket.nickname = 'user' + clientCount
+  io.emit('enter', socket.nickname + " comes in")
+
+  socket.on('message', function(str) {
+    io.emit('message', socket.nickname + ' says: ' + str)
+  })
+
+  socket.on('disconnect', function() {
+    io.emit('leave', socket.nickname + " has left")
+  })
+})
+
+console.log("websocket server listening on port: " + PORT);
